b94ebb0229832af31e86bd367d7174f52729a457
Johnny <john@Richies-iMac.local>
2023-05-14 15:24:52 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const startButton = document.querySelector('.start');
+const stopButton = document.querySelector('.stop');
+const resetButton = document.querySelector('.reset');
+
+const seconds = document.querySelector('.seconds');
+const minutes = document.querySelector('.minutes');
+const hours = document.querySelector('.hours');
+
+let hrs = 00;
+let min = 00;
+let second = 00;
+let timer = true;
+
+startButton.addEventListener('click', () => {
+
+  console.log(timer)
+})
+
+stopButton.addEventListener('click', () => {
+
+  console.log('stop button')
+})
+
+resetButton.addEventListener('click', () => {
+
+  console.log('reset button')
+})
+
+function startWatch(){ 
+  if(timer){
+    
+  }
+}
